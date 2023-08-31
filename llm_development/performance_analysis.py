391b1177c37cc3ea9e6272a1e0428ef41ddeff40
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 13:41:22 +0500

    Update performance_analysis.py

--- llm_development/performance_analysis.py
+++ llm_development/performance_analysis.py
@@ -1,1 +1,17 @@
+def compare_with_samples(question, model_answer, sample_answer):
+    """
+    Compare the model's answer with the sample answer.
+    This is a placeholder function and might require more advanced comparison techniques.
+    """
+    return model_answer == sample_answer
+
+def measure_performance(questions, model_answers, sample_answers):
+    """
+    Measure the performance of the model based on a list of questions, model's answers, and sample answers.
+    """
+    correct_answers = 0
+    for q, ma, sa in zip(questions, model_answers, sample_answers):
+        if compare_with_samples(q, ma, sa):
+            correct_answers += 1
+    return correct_answers / len(questions) if questions else 0
  
