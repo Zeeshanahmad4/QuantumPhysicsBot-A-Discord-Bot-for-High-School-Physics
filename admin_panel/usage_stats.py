cb9de30afc3b6dfc042201f467b14494b6d159a3
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 13:46:58 +0500

    Update usage_stats.py

--- admin_panel/usage_stats.py
+++ admin_panel/usage_stats.py
@@ -1,1 +1,28 @@
+class UsageStats:
+    def __init__(self):
+        # Placeholder for the data structure storing usage stats
+        self.data = {}
+
+    def record_message(self, message):
+        """
+        Record a message interaction with the LLM.
+        Placeholder function, actual implementation will vary based on the LLM.
+        """
+        # Placeholder implementation, to be expanded upon actual integration
+        if message not in self.data:
+            self.data[message] = 1
+        else:
+            self.data[message] += 1
+
+    def get_statistics(self):
+        """
+        Return the current LLM usage statistics.
+        Placeholder function, will require actual LLM integration for detailed stats.
+        """
+        # Placeholder stats, will need actual data integration
+        return {
+            "total_messages": sum(self.data.values()),
+            "unique_questions": len(self.data),
+            "most_common_question": max(self.data, key=self.data.get, default=None)
+        }
  
