bdc43782fadeb4febe94876223e1371012351166
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 13:42:13 +0500

    Update knowledge_base.py

--- llm_development/knowledge_base.py
+++ llm_development/knowledge_base.py
@@ -1,1 +1,14 @@
+class KnowledgeBase:
+    def __init__(self):
+        self.kb = {}
+
+    def add_qa_pair(self, question, answer):
+        self.kb[question] = answer
+
+    def get_answer(self, question):
+        return self.kb.get(question, None)
+
+    def update_answer(self, question, new_answer):
+        if question in self.kb:
+            self.kb[question] = new_answer
  
