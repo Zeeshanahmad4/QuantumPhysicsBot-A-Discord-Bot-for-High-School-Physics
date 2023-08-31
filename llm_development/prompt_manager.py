c305c3b4fb2149a6ff889546d3f2eb22682e210b
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 13:41:45 +0500

    Update prompt_manager.py

--- llm_development/prompt_manager.py
+++ llm_development/prompt_manager.py
@@ -1,1 +1,14 @@
+class PromptManager:
+    def __init__(self):
+        self.prompts = []
+
+    def add_prompt(self, prompt):
+        self.prompts.append(prompt)
+
+    def modify_prompt(self, index, new_prompt):
+        if 0 <= index < len(self.prompts):
+            self.prompts[index] = new_prompt
+
+    def get_prompt(self, index):
+        return self.prompts[index] if 0 <= index < len(self.prompts) else None
  
